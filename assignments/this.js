bda43147d3427ccd4e53857eeae3e491cff1871a
Ray Aguilar <ray.aguilar15@yahoo.com>
2019-07-09 18:35:34 -0500

    created prinicple example for Implicit Binding

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window/Global Object Binding: "this" is given the value of the window/console
-* 2. Implicit Binding
+* 2. Implicit Binding: Whatever is to the left of a dot when calling a function, that is "this". 
 * 3. Explicit Binding
 * 4. New Binding
 *
@@ -10,11 +10,18 @@
 */
 
 // Principle 1
-console.log(this);
+// console.log(this);
 // code example for Window Binding
 
 // Principle 2
-
+ const me = {
+     name: 'Ray',
+     age: 23,
+     sayGreeting: () => {
+        console.log(`Hello! My name is ${this.name}. I'm ${this.age} years old.`)
+     }
+ }
+me.sayGreeting();
 // code example for Implicit Binding
 
 // Principle 3
