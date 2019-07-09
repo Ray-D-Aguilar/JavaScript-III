2d4ca788d57370a191ea18bf358e174421f56ebe
Ray Aguilar <ray.aguilar15@yahoo.com>
2019-07-09 18:16:30 -0500

    beginning to answer principle questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding: "this" is given the value of the window/console
+* 2. Implicit Binding
+* 3. Explicit Binding
+* 4. New Binding
 *
 * write out a code example of each explanation above
 */
