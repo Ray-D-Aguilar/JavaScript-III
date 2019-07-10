40290eae39966c55057f1799274ae0e1fcfb5080
Ray Aguilar <ray.aguilar15@yahoo.com>
2019-07-09 20:17:30 -0500

    created example for new binding

--- assignments/this.js
+++ assignments/this.js
@@ -4,7 +4,8 @@
 * 1. Window/Global Object Binding: "this" is given the value of the window/console
 * 2. Implicit Binding: Whatever is to the left of a dot when calling a function, that is "this". 
 * 3. Explicit Binding
-* 4. New Binding
+* 4. New Binding: when a constructor function is used, you use the "new" keyword to 
+implement and apply your newly created properties to a "this"/object.
 *
 * write out a code example of each explanation above
 */
@@ -14,14 +15,16 @@
 // code example for Window Binding
 
 // Principle 2
- const me = {
-     name: 'Ray',
-     age: 23,
-     sayGreeting: () => {
-        console.log(`Hello! My name is ${this.name}. I'm ${this.age} years old.`)
-     }
- }
-me.sayGreeting();
+const me = {
+    name: 'Ray',
+    age: 23,
+    sayGreeting: function(greeting) {
+       console.log(`${greeting} My name is ${this.name}. I'm ${this.age} years old.`)
+       console.log(this)
+    }
+}
+me.sayGreeting("Hello! ");
+
 // code example for Implicit Binding
 
 // Principle 3
@@ -29,5 +32,18 @@ me.sayGreeting();
 // code example for New Binding
 
 // Principle 4
+ function Person(hugger) {
+     this.greeting = 'Hello ';
+     this.hugger = hugger;
+     this.speak = () => {
+         console.log(this.greeting + this.hugger);
+         console.log(this);
+     }
+ }
 
+ const ray = new Person('Tiah');
+ const tiah = new Person('Ray');
+
+ray.speak();
+tiah.speak();
 // code example for Explicit Binding
