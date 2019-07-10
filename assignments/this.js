baa2bb9d02d168fb1ea22909b843a48b455c88f1
Ray Aguilar <ray.aguilar15@yahoo.com>
2019-07-09 20:23:16 -0500

    added example of new binding

--- assignments/this.js
+++ assignments/this.js
@@ -1,9 +1,17 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
-*
+*    
+
+        WINDOW BINDING
 * 1. Window/Global Object Binding: "this" is given the value of the window/console
-* 2. Implicit Binding: Whatever is to the left of a dot when calling a function, that is "this". 
-* 3. Explicit Binding
+
+        IMPLICIT BINDING
+* 2. Implicit Binding: Whatever is to the left of a dot when calling a function, that is "this".
+
+        EXPLICIT BINDING
+* 3. Explicit Binding: This is defined when the call or apply method is used.
+        
+NEW BINDING
 * 4. New Binding: when a constructor function is used, you use the "new" keyword to 
 implement and apply your newly created properties to a "this"/object.
 *
@@ -11,7 +19,7 @@ implement and apply your newly created properties to a "this"/object.
 */
 
 // Principle 1
-// console.log(this);
+ console.log(this);
 // code example for Window Binding
 
 // Principle 2
@@ -28,22 +36,22 @@ me.sayGreeting("Hello! ");
 // code example for Implicit Binding
 
 // Principle 3
+function Person(hugger) {
+    this.greeting = 'Hello ';
+    this.hugger = hugger;
+    this.speak = () => {
+        console.log(this.greeting + this.hugger);
+        console.log(this);
+    }
+}
 
-// code example for New Binding
-
-// Principle 4
- function Person(hugger) {
-     this.greeting = 'Hello ';
-     this.hugger = hugger;
-     this.speak = () => {
-         console.log(this.greeting + this.hugger);
-         console.log(this);
-     }
- }
-
- const ray = new Person('Tiah');
- const tiah = new Person('Ray');
+const ray = new Person('Tiah');
+const tiah = new Person('Ray');
 
 ray.speak();
 tiah.speak();
+// code example for New Binding
+
+// Principle 4
+ 
 // code example for Explicit Binding
