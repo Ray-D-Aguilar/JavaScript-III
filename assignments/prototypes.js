257ece5c706c7818e15770ba243131e3e0d4f2d2
Ray Aguilar <ray.aguilar15@yahoo.com>
2019-07-10 20:36:45 -0500

    Created a Constructor function for GameObject

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -7,7 +7,15 @@
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
+  function GameObject(attributes) {
+    this.createdAt = attributes.createdAt;
+    this.name = attributes.name;
+    this.dimensions = attributes.dimensions;
+  }
+
+  GameObject.prototype.destroy = function () {
+    return `${this.name} was removed from the game.`
+  }
 /*
   === GameObject ===
   * createdAt
@@ -15,6 +23,7 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+
 
 /*
   === CharacterStats ===
@@ -41,7 +50,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +111,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
