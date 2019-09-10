e41cd79f97d273f24ae067ea37396aa9914062b3
Ray Aguilar <ray.aguilar15@yahoo.com>
2019-09-10 17:59:12 -0500

    finished the four questions on the different types of bindings

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding: "this" is given the value of the window/console
+* 2. Implicit Binding: Whatever is to the left of the dot when calling a function, that is "this".
+* 3. Explicit Binding: This is defined when the call or apply method is used.
+* 4. New Binding: When a constructor function is used, you use the "new" keyword to implement and apply your newly created properties to a "this"/object.
 *
 * write out a code example of each explanation above
 */
